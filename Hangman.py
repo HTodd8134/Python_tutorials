3f45ece242008bf1d176706a82b4505913a0603b
HTodd8134 <harryalexandertodd@gmail.com>
2023-08-03 12:02:14 +0100

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,7 +1,7 @@
 import random as rand
 
-guess_count = 0
-guess_limit = 10
+guess_count = 10
+guess_limit = 0
 out_of_guesses = False
 guess = " "
 number = rand.randint(1, 66)
@@ -24,15 +24,21 @@ secret_word = word_list[int(number)]
 
 
 while guess != secret_word and not(out_of_guesses):
-    if guess_count < guess_limit:
+    for letter in secret_word:
+        if letter in guess:
+            print(letter)
+        else:
+            print("_")
+    if guess_count > guess_limit:
         guess = input("pick a letter: ")
-        guess_count += 1
+        guess_count -= 1
+        print("you have " + str(guess_count) + " more guesses")
     else:
         out_of_guesses = True
     if guess in secret_word:
-        print("closer")
+        print("yeah")
     else:
-        print("nah")
+        print("wrong")
 if guess == secret_word:
     print("you won!")
 else:
