a1f2ab8e7105ad8aa4d8ddd3e7160d2affc2a9d1
HTodd8134 <harryalexandertodd@gmail.com>
2023-08-08 16:45:04 +0100

    test 3

--- watch/TK_stopclock_mk2.py
+++ watch/TK_stopclock_mk2.py
@@ -8,7 +8,7 @@ root.geometry("300x170")
 text_result = tk.Text(root, height=2, width=25)
 text_result.grid(columnspan=5)
 # Nicolas  
-#btn_÷//start = tk.Button(root, width=7, text="start")
+#btn_÷//start =    tk.Button(root, width=7, text="start")
 #btn_start.grid(row=2, column=0)
 #btn_stop = tk.Button(root, width=7, text="stop")
 #btn_stop.grid(row=2, column=1)
